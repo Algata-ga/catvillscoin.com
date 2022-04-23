4cd61904064ba38bc8dc83e766a9aabc0fb981a6
KKharidev <kkharidev@gmail.com>
2022-04-23 20:17:44 +0530

    ..

--- src/sections/Latest/Latest.jsx
+++ src/sections/Latest/Latest.jsx
@@ -1,4 +1,4 @@
-import {  Heading, Line } from "../../components";
+import { Heading, Line } from "../../components";
 import style from "./Latest.module.css";
 
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -16,78 +16,78 @@ const medium = [
         "name": 'A',
         "image_url": logo,
         "title": 'hello there',
-        "content" : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem exercitationem voluptatum minus quae eaque vitae tempore.',
+        "content": 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem exercitationem voluptatum minus quae eaque vitae tempore.',
         "id": 0,
     },
     {
         "name": 'B',
         "image_url": logo,
         "title": 'hello there',
-        "content" : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem exercitationem voluptatum minus quae eaque vitae tempore.',
+        "content": 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem exercitationem voluptatum minus quae eaque vitae tempore.',
 
         "id": 1,
     }, {
         "name": 'C',
         "image_url": logo,
         "title": 'hello there',
-        "content" : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem exercitationem voluptatum minus quae eaque vitae tempore.',
+        "content": 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem exercitationem voluptatum minus quae eaque vitae tempore.',
 
         "id": 2,
     }, {
         "name": 'D',
         "image_url": logo,
         "title": 'hello there',
-        "content" : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem exercitationem voluptatum minus quae eaque vitae tempore.',
+        "content": 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem exercitationem voluptatum minus quae eaque vitae tempore.',
 
         "id": 3,
     }, {
         "name": 'E',
         "image_url": logo,
         "title": 'hello there',
-        "content" : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem exercitationem voluptatum minus quae eaque vitae tempore.',
+        "content": 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem exercitationem voluptatum minus quae eaque vitae tempore.',
 
         "id": 4,
     }
 ];
 
-const Latest=()=>{
-    return(
+const Latest = () => {
+    return (
         <section className={style.latest}>
             <Container>
 
-            <div className={style.contain}>
+                <div className={style.contain}>
 
-                <Heading heading="Latest " change="Activity" />
-                <Swiper className={style.swiper + " " + "mySwiper"}
-                    effect={"coverflow"}
-                    grabCursor={false}
-                    centeredSlides={true}
-                    slidesPerView={"auto"}
+                    <Heading heading="Latest " change="Activity" />
+                    <Swiper className={style.swiper + " " + "mySwiper"}
+                        effect={"coverflow"}
+                        grabCursor={false}
+                        centeredSlides={true}
+                        slidesPerView={"auto"}
 
-                    autoplay={{
-                        delay: 2500,
-                        disableOnInteraction: true,
-                    }}
-                    coverflowEffect={{
-                        rotate: 0,
-                        stretch: 0,
-                        depth: 0,
-                        modifier: 1,
-                    }}
-                    modules={[Autoplay, EffectCoverflow, Pagination]}
-                >
-                    {medium.map((item) => 
-                        <SwiperSlide className={style.slider}>
-                            <div className={style.imgbox}>
-                            <img src={item.image_url} />
-                            </div>
-                            <h5>{item.title}</h5>
-                            <p>{item.content}</p>
-                        </SwiperSlide>
-                )}
-                </Swiper>
-            </div>
-            <Line />
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: false,
+                        }}
+                        coverflowEffect={{
+                            rotate: 0,
+                            stretch: 0,
+                            depth: 0,
+                            modifier: 1,
+                        }}
+                        modules={[Autoplay, EffectCoverflow, Pagination]}
+                    >
+                        {medium.map((item) =>
+                            <SwiperSlide key={item.id} className={style.swiperslide}     >
+                                <div className={style.imgbox}>
+                                    <img src={item.image_url} />
+                                </div>
+                                <h5>{item.title}</h5>
+                                <p>{item.content}</p>
+                            </SwiperSlide>
+                        )}
+                    </Swiper>
+                </div>
+                <Line />
             </Container>
         </section>
     );
