3617dec901c0bd041338bc2482806cd0faf50426
Alfred Jophy <alfredjophy@protonmail.com>
2022-04-24 19:26:48 +0530

    added bio modal

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,4 @@ export { default as Leftbtn } from "./Leftbtn/Leftbtn";
 export { default as Heading } from "./Heading/Heading";
 export { default as Line } from "./Line/Line";
 export { default as Footer } from "./Footer/Footer";
-
+export { default as Modal } from "./modal/Modal";
